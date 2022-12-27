00efa74ba9621615b39ac73aa835f49cd0ac68eb
JensGrundtmann <121521622+JensGrundtmann@users.noreply.github.com>
2022-12-28 00:12:54 +0100

    et kompas med lys styrke som hjælper med at finde N S Ø og V

--- main.ts
+++ main.ts
@@ -5,16 +5,25 @@ basic.forever(function () {
     if (retning >= 315 && retning <= 360 || retning >= 0 && retning <= 45) {
         basic.showString("N")
     } else if (retning > 45 && retning <= 135) {
+        led.setBrightness(Math.map(retning, 45, 90, 50, 255))
+        led.setBrightness(Math.map(retning, 90, 135, 255, 50))
         if (retning == 90) {
-            led.setBrightness(255)
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # # # # #
+                . . # . .
+                . . # . .
+                `)
+        } else {
+            basic.showLeds(`
+                # # # # #
+                # . . # #
+                # . # . #
+                # # . . #
+                # # # # #
+                `)
         }
-        basic.showLeds(`
-            # # # # #
-            # . . # #
-            # . # . #
-            # # . . #
-            # # # # #
-            `)
     } else if (retning > 135 && retning <= 225) {
         basic.showString("S")
     } else if (retning > 225 && retning <= 315) {
